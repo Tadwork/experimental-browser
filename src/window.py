2b8c84e213d402fd5c89e525f2dd6484a9d3be2c
Tzvi Friedman <tadwork@gmail.com>
2023-07-04 14:17:56 -0400

    add a font cache

--- src/window.py
+++ src/window.py
@@ -4,7 +4,7 @@
 from dataclasses import dataclass
 
 import tkinter as tk
-import tkinter.font as tkfont
+from src.fonts import get_font
 from src.connection import parse_url, request
 
 HSTEP, VSTEP = 13, 18
@@ -27,22 +27,29 @@ class Tag:
 
 
 class Layout:
+    """The layout engine for the browser"""
+
     display_list = []
     cursor_x = HSTEP
     cursor_y = VSTEP
-    weight = "normal"
-    style = "roman"
-    family = "Times New Roman"
-    size = 14
     line = []
 
     def __init__(self, tokens, browser) -> None:
         self.browser = browser
+        self.weight = browser.default_font["weight"]
+        self.style = browser.default_font["slant"]
+        self.family = browser.default_font["family"]
+        self.size = browser.default_font["size"]
         for tok in tokens:
             self.token(tok)
         self.flush()
 
     def tag(self, tok):
+        """make changes to the display list based on the tag
+
+        Args:
+            tok (str): the token to process
+        """
         if tok.tag == "b":
             self.weight = "bold"
         elif tok.tag == "/b":
@@ -72,12 +79,8 @@ class Layout:
             self.cursor_y += VSTEP
 
     def text(self, tok):
-        font = tkfont.Font(
-            family=self.family,
-            size=self.size,
-            weight=self.weight,
-            slant=self.style,
-        )
+        """adds text to the display list"""
+        font = get_font(self.family, self.size, self.weight, self.style)
         for word in tok.text.split():
             w = font.measure(word)
             if self.cursor_x + w > self.browser.width - HSTEP:
@@ -85,8 +88,9 @@ class Layout:
             self.line.append((self.cursor_x, word, font))
             # add the width of the word and a space
             self.cursor_x += w + font.measure(" ")
-            
+
     def flush(self):
+        """flush the current line to the display list"""
         if not self.line:
             return
         metrics = [font.metrics() for x, word, font in self.line]
@@ -99,7 +103,7 @@ class Layout:
         self.line = []
         max_descent = max([metric["descent"] for metric in metrics])
         self.cursor_y = baseline + 1.25 * max_descent
-        
+
     def token(self, tok):
         if isinstance(tok, Tag):
             self.tag(tok)
@@ -150,6 +154,7 @@ class Browser:
         self.window.bind("<Down>", self.scroll)
         self.window.bind("<Up>", self.scroll)
         self.canvas = tk.Canvas(self.window, width=width, height=height)
+        self.default_font = get_font("Times New Roman", 14, "normal", "roman")
         self.canvas.pack()
 
     def scroll(self, event):
