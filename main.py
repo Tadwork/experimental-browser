24e356cea037c8f45db8ca9a24b2f25898a0bee3
Tzvi Friedman <tadwork@gmail.com>
2023-07-04 00:32:33 -0400

    Add https support and docstrings

--- main.py
+++ main.py
@@ -1,31 +1,62 @@
+"""
+This is a simple HTTP client that can download a web page from a URL.
+"""
 import socket
 import argparse
+import ssl
 
 # https://browser.engineering/http.html
 
 args = argparse.ArgumentParser()
 args.add_argument("--url", help="The URL to download")
 
-def parse_url(url):
-    assert url.startswith("http://")
-    url = url[len("http://"):]
+def parse_url(url:str):
+    """ parses a url into its components
+
+    Args:
+        url (str): url to parse
+
+    Returns:
+        scheme (str): http or https
+        host (str): the host name
+        port (int): the port number
+        path (str): the rest of the path starting from /
+    """
+    scheme, url = url.split("://", 1)
+    assert scheme in ("http", "https"), f"Unknown scheme {scheme}"
     host,path = url.split("/",1)
     path = "/" + path  # add back the leading slash
-    return host, path
+    port = 443 if scheme == "https" else 80
+    if ":" in host:
+        host, p = host.split(":", 1)
+        port = int(p)
+    return scheme, host, port, path
 
-def request(host,path):
-    s = socket.socket(
-        family=socket.AF_INET,
-        type=socket.SOCK_STREAM,
-        proto=socket.IPPROTO_TCP,
-    )
-    s.connect((host, 80))
-    s.send("GET {} HTTP/1.0\r\n".format(path).encode("utf8") + 
-    "Host: {}\r\n\r\n".format(host).encode("utf8"))
-    response = s.makefile("r", encoding="utf8", newline="\r\n")
+def get_page(sock:str,host:str,port:int,path:str):
+    """ uses a socket to get a page
+
+    Args:
+        sock (str): the socket to use
+        host (str): the host name
+        port (int): the port number
+        path (str): the rest of the path 
+
+    Returns:
+        version (str): the http version
+        status (str): the status code
+        explanation (str): the explanation of the status code
+        headers (dict): the headers
+        body (str): the body of the response
+    """
+    sock.connect((host, port))
+    sock.send(
+        f"GET {path} HTTP/1.0\r\n".encode("utf8") +
+        f"Host: {host}\r\n\r\n".encode("utf8")
+        )
+    response = sock.makefile("r", encoding="utf8", newline="\r\n")
     statusline = response.readline()
     version, status, explanation = statusline.split(" ", 2)
-    assert status == "200", "{}: {}".format(status, explanation)
+    assert status == "200", f"{status}: {explanation}"
     headers = {}
     while True:
         line = response.readline()
@@ -35,10 +66,41 @@ def request(host,path):
     assert "transfer-encoding" not in headers
     assert "content-encoding" not in headers
     body = response.read()
-    s.close()
     return version, status, explanation, headers, body
+
+def request(scheme:str,host:str,port:int,path:str):
+    """ makes an http request
+
+    Args:
+        scheme (str): http or https
+        host (str): the host name
+        port (int): port number
+        path (str): the rest of the path
+
+    Returns:
+        values from get_path
+    """
+    with socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    ) as sock:
+        if scheme == "https":
+            ctx = ssl.create_default_context()
+            with ctx.wrap_socket(sock, server_hostname=host) as ssock:
+                return get_page(ssock,host,port,path)
+        else:
+            return get_page(sock,host,port,path)
     
-def display(body):
+def display(body:str):
+    """ strips html tags from the body of the response and returns the text
+
+    Args:
+        body (str): the body of the response
+
+    Returns:
+        str: the body of the response without html tags
+    """
     text = []
     in_angle = False
     for c in body:
@@ -51,9 +113,11 @@ def display(body):
     return "".join(text)
     
 def main():
+    """ main function
+    """
     url = args.parse_args().url
-    host,path = parse_url(url)
-    version,status,explanation,headers, body = request(host,path)
+    scheme,host,port, path = parse_url(url)
+    version,status,explanation,headers, body = request(scheme,host,port,path)
     print(display(body))
 
 if __name__ == "__main__":
