539c9b3905a70fc8c84d651853ac4f6445b1df41
Tzvi Friedman <tadwork@gmail.com>
2023-07-04 12:14:29 -0400

    Add layout class and tokenizer

--- src/window.py
+++ src/window.py
@@ -10,37 +10,97 @@ from src.connection import parse_url, request
 HSTEP, VSTEP = 13, 18
 SCROLL_STEP = 100
 
+
 @dataclass
 class Text:
     """A wrapper for text to display"""
+
     text: str
-    
+
+
 @dataclass
 class Tag:
     """A wrapper for html tags"""
+
     tag: str
     attributes: dict = None
 
 
+class Layout:
+    display_list = []
+    cursor_x = HSTEP
+    cursor_y = VSTEP
+    weight = "normal"
+    style = "roman"
+    size = 14
+
+    def __init__(self, tokens, browser) -> None:
+        self.browser = browser
+        for tok in tokens:
+            self.token(tok)
+
+    def token(self, tok):
+        font = tkfont.Font(
+            family="Times New Roman",
+            size=self.size,
+            weight=self.weight,
+            slant=self.style,
+        )
+        if isinstance(tok, Tag):
+            if tok.tag == "b":
+                self.weight = "bold"
+            elif tok.tag == "/b":
+                self.weight = "normal"
+            elif tok.tag == "i":
+                self.style = "italic"
+            elif tok.tag == "/i":
+                self.style = "roman"
+            elif tok.tag == 'small':
+                self.size -= 2
+            elif tok.tag == '\small':
+                self.size += 2
+            elif tok.tag == 'big':
+                self.size += 4
+            elif tok.tag == '\big':
+                self.size -= 4
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                w = font.measure(word)
+                if self.cursor_x + w > self.browser.width - HSTEP:
+                    self.cursor_y += font.metrics("linespace") * 1.25
+                    self.cursor_x = HSTEP
+                self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+                # re-add the whitespace
+                self.cursor_x += w + font.measure(" ")
+
+
 def lex(body: str):
-    """strips html tags from the body of the response and returns the text
+    """strips html tags from the body of the response and returns the text and tags
 
     Args:
         body (str): the body of the response
 
     Returns:
-        str: the body of the response without html tags
+        list: an array of tags and text
     """
-    text = []
-    in_angle = False
+    out = []
+    text = ""
+    in_tag = False
     for c in body:
         if c == "<":
-            in_angle = True
+            in_tag = True
+            if text:
+                out.append(Text(text))
+                text = ""
         elif c == ">":
-            in_angle = False
-        elif not in_angle:
-            text.append(c)
-    return "".join(text)
+            in_tag = False
+            out.append(Tag(text))
+            text = ""
+        else:
+            text += c
+    if not in_tag and text:
+        out.append(Text(text))
+    return out
 
 
 class Browser:
@@ -71,41 +131,20 @@ class Browser:
             self.scroll_start -= SCROLL_STEP
         self.draw()
 
-    def layout(self, text):
-        """generate a layout list from the text
-
-        Args:
-            text (str): the text to layout
-
-        Returns:
-            list: list of tuples of the form (x, y, c)
-            where x and y are the coordinates of the character c
-        """
-        tnr_font = tkfont.Font(family="Times New Roman", size=14)
-        display_list = []
-        cursor_x, cursor_y = HSTEP, VSTEP
-        for word in text.split():
-            w = tnr_font.measure(word)
-            if cursor_x + w > self.width - HSTEP:
-                cursor_y += tnr_font.metrics("linespace") * 1.25
-                cursor_x = HSTEP
-            display_list.append((cursor_x, cursor_y, word))
-            #readd the whitespace
-            cursor_x += w + tnr_font.measure(" ")
-        return display_list
-
     def draw(self):
         """draw the display list on the canvas"""
         self.canvas.delete("all")
-        for x, y, c in self.display_list:
+        for x, y, word, font in self.display_list:
             if y > self.scroll_start + self.height:
                 continue
             if y + VSTEP < self.scroll_start:
                 continue
-            self.canvas.create_text(x, y - self.scroll_start, text=c , anchor="nw")
+            self.canvas.create_text(
+                x, y - self.scroll_start, text=word, font=font, anchor="nw"
+            )
 
     def load(self, url):
-        """ load a url into the browser
+        """load a url into the browser
 
         Args:
             url (URL): the url to load
@@ -113,5 +152,5 @@ class Browser:
         parsed_url = parse_url(url)
         response = request(parsed_url)
         text = lex(response.body)
-        self.display_list = self.layout(text)
+        self.display_list = Layout(text, browser=self).display_list
         self.draw()
--- test/test_window.py
+++ test/test_window.py
@@ -1,21 +1,21 @@
 import pytest
 
-from src.window import lex
+from src.window import lex, Text, Tag
 
 def test_lex_no_tags():
-    assert lex("hello world") == "hello world"
+    assert lex("hello world") == [Text("hello world")]
 
 def test_lex_single_tag():
-    assert lex("<p>hello world</p>") == "hello world"
+    assert lex("<p>hello world</p>") == [Tag("p"),Text("hello world"),Tag("/p")]
 
 def test_lex_nested_tags():
-    assert lex("<p><b>hello</b> world</p>") == "hello world"
+    assert lex("<p><b>hello</b> world</p>") == [Tag("p"),Tag("b"),Text("hello"),Tag("/b"),Text(" world"),Tag("/p")]
 
 def test_lex_empty_tag():
-    assert lex("<p></p>") == ""
+    assert lex("<p></p>") == [Tag("p"),Tag("/p")]
 
 def test_lex_multiple_tags():
-    assert lex("<p>hello</p><p>world</p>") == "helloworld"
+    assert lex("<p>hello</p><p>world</p>") == [Tag("p"),Text("hello"),Tag("/p"),Tag("p"),Text("world"),Tag("/p")]
 
 # TODO: fix this test so these characters are unescaped
 # def test_lex_special_characters():
